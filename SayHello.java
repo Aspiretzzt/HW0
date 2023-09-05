bf5dfae3964a9e505b64cb9a667d8981ad5b2ca0
Aspiretzzt <144132563+Aspiretzzt@users.noreply.github.com>
2023-09-05 09:29:10 -0400

    Add files via upload

---
+++ SayHello.java
@@ -0,0 +1,14 @@
+package HW0;
+/*
+ * Zhongtian Zhai
+ * zzhai4@u.rochester.edu
+ * Sep 5th, 2023
+ * HW0
+ */
+public class SayHello {
+	public static void main(String[] args) {
+		System.out.println("Zhongtian Zhai");
+		System.out.println("zzhai4@u.rochester.edu");
+		System.out.println("I am a sophomore new to computer science");
+	}
+}
